6efd1969707951d900b00b7b3222d5ae91c3054f
Fahri MacBook <fahri-can@web.de>
2018-05-14 19:50:35 +0200

    Initial Commit

---
+++ User Defaults-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  User Defaults-App
+//
+//  Created by fahri on 14.05.18.
+//  Copyright © 2018 fahri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
